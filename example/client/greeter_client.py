8cbb06a90d265aad0e36df49b62062884ef0229f
yumuranishi <67744349+halroot-dot@users.noreply.github.com>
2022-07-13 09:10:00 +0000

    WIP commit : 一時的にPC上でsslを使ってgRPCが出来る事を確認した [#1]

--- example/client/greeter_client.py
+++ example/client/greeter_client.py
@@ -22,11 +22,20 @@ import helloworld_pb2
 import helloworld_pb2_grpc
 
 
+# def run():
+#     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+#     # used in circumstances in which the with statement does not fit the needs
+#     # of the code.
+#     with grpc.insecure_channel('localhost:8080') as channel:
+#         stub = helloworld_pb2_grpc.GreeterStub(channel)
+#         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+#     print("Greeter client received: " + response.message)
+
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    with grpc.insecure_channel('localhost:8080') as channel:
+    root = open("../ssl/root.crt", "rb").read()
+    channel_credential = grpc.ssl_channel_credentials(root_certificates=root)
+
+    with grpc.secure_channel('localhost:51011', channel_credential) as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
