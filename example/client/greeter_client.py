3195b7321ae7b83f0dfd7e1c678e8a794092bf79
yumuranishi <67744349+halroot-dot@users.noreply.github.com>
2022-07-15 06:54:37 +0000

    Enable to use envoy proxy [gRPC proxy経由で通信できるか確認 [#1]
    client --(normal)-- envoy --(normal)-- server

--- example/client/greeter_client.py
+++ example/client/greeter_client.py
@@ -22,23 +22,23 @@ import helloworld_pb2
 import helloworld_pb2_grpc
 
 
-# def run():
-#     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-#     # used in circumstances in which the with statement does not fit the needs
-#     # of the code.
-#     with grpc.insecure_channel('localhost:8080') as channel:
-#         stub = helloworld_pb2_grpc.GreeterStub(channel)
-#         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-#     print("Greeter client received: " + response.message)
-
 def run():
-    root = open("../ssl/root.crt", "rb").read()
-    channel_credential = grpc.ssl_channel_credentials(root_certificates=root)
-
-    with grpc.secure_channel('localhost:1443', channel_credential) as channel:
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:8080') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
+
+# def run():
+#     root = open("../ssl/root.crt", "rb").read()
+#     channel_credential = grpc.ssl_channel_credentials(root_certificates=root)
+
+#     with grpc.secure_channel('localhost:1443', channel_credential) as channel:
+#         stub = helloworld_pb2_grpc.GreeterStub(channel)
+#         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+#     print("Greeter client received: " + response.message)
 
 
 if __name__ == '__main__':
